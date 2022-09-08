a87d05163083025d0d5c27229df6a049b93e9921
JGHolscher <holschjg@mail.uc.edu>
2022-09-08 13:21:50 -0400

    ..

--- src/Worker.java
+++ src/Worker.java
@@ -2,6 +2,6 @@ public class Worker extends Person{
     double hourlyPayRate;
 
     public Worker(String fName, String lName, String title, int YOB) {
-        super(fName, lName, title, YOB);
+        super(fName, lName, title, YOB);/// maybe don't need this
     }
 }
